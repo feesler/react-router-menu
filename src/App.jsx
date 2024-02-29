9682b5c5fc8570ef3ddd8a5e2e4325bb25bee894
Ilya Makarov <feesler@gmail.com>
2024-02-29 23:58:33 +0300

    * Migrated to react-router v6

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Menu from './components/Menu/Menu.jsx';
 import HomePage from './components/HomePage/HomePage.jsx';
 import DriftPage from './components/DriftPage/DriftPage.jsx';
@@ -13,10 +13,12 @@ export default function App() {
       <div>
         <Menu />
         <div className="page">
-          <Route path="/" exact component={HomePage} />
-          <Route path="/drift" component={DriftPage} />
-          <Route path="/timeattack" component={TimeAttackPage} />
-          <Route path="/forza" component={ForzaPage} />
+          <Routes>
+            <Route path="/" Component={HomePage} />
+            <Route path="/drift" Component={DriftPage} />
+            <Route path="/timeattack" Component={TimeAttackPage} />
+            <Route path="/forza" Component={ForzaPage} />
+          </Routes>
         </div>
       </div>
     </Router>
